3dcac274af77e84962cf5d1f43fb271d8bced230
Mubashir Hasan <mubashirhasan716@gmail.com>
2020-10-12 11:54:04 +0530

    ee

---
+++ .history/src/App_20200925085323.vue
@@ -0,0 +1,18 @@
+<template>
+<div id="app" class="antialiased text-gray-800">
+    <header class="relative bg-no-repeat bg-cover lg:bg-center p-6 bg-top-right bi">
+
+    </header>
+</div>
+</template>
+
+<script>
+export default {
+    name: 'App',
+
+}
+</script>
+
+<style>
+ackground-image: url('../public/airbnb-home.jpg');
+</style>
